60e9b1abba24d5213229d6e18da83136133a9703
h4ndzdatm0ld <hugotinoco@icloud.com>
2023-05-29 07:30:27 +0000

    format

--- lib/nautobot-fargate-ecs-stack.ts
+++ lib/nautobot-fargate-ecs-stack.ts
@@ -1,31 +1,47 @@
-import { Stack, StackProps, Duration } from 'aws-cdk-lib';
-import { SubnetType } from 'aws-cdk-lib/aws-ec2';
-import { Cluster, ContainerImage, FargateTaskDefinition, LogDrivers, FargateService, Protocol } from 'aws-cdk-lib/aws-ecs';
-import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import { Construct } from 'constructs';
-import { NautobotDockerImageStack } from './nautobot-docker-image-stack';
-import { NginxDockerImageStack } from './nginx-docker-image-stack';
-import { NautobotSecretsStack } from './nautobot-secrets-stack';
-import { NautobotVpcStack } from './nautobot-vpc-stack';
-import { NautobotDbStack } from './nautobot-db-stack';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
-import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps, Duration } from "aws-cdk-lib";
+import { SubnetType } from "aws-cdk-lib/aws-ec2";
+import {
+  Cluster,
+  ContainerImage,
+  FargateTaskDefinition,
+  LogDrivers,
+  FargateService,
+  Protocol,
+} from "aws-cdk-lib/aws-ecs";
+import { ApplicationProtocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Construct } from "constructs";
+import { NautobotDockerImageStack } from "./nautobot-docker-image-stack";
+import { NginxDockerImageStack } from "./nginx-docker-image-stack";
+import { NautobotSecretsStack } from "./nautobot-secrets-stack";
+import { NautobotVpcStack } from "./nautobot-vpc-stack";
+import { NautobotDbStack } from "./nautobot-db-stack";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import { Role, ServicePrincipal, ManagedPolicy } from "aws-cdk-lib/aws-iam";
 import { DnsRecordType } from "@aws-cdk/aws-servicediscovery";
 
 export class NautobotFargateEcsStack extends Stack {
-  constructor(scope: Construct, id: string, dockerStack: NautobotDockerImageStack, nginxStack: NginxDockerImageStack, secretsStack: NautobotSecretsStack, vpcStack: NautobotVpcStack, dbStack: NautobotDbStack, props?: StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    dockerStack: NautobotDockerImageStack,
+    nginxStack: NginxDockerImageStack,
+    secretsStack: NautobotSecretsStack,
+    vpcStack: NautobotVpcStack,
+    dbStack: NautobotDbStack,
+    props?: StackProps
+  ) {
     super(scope, id, props);
 
     // variables from other stacks
     const vpc = vpcStack.vpc;
     const alb = vpcStack.alb;
     const nautobotSecurityGroup = vpcStack.nautobotSecurityGroup;
-    const namespace: string = 'nautobot-service.local';
+    const namespace: string = "nautobot-service.local";
 
-    const cluster = new Cluster(this, 'NautobotCluster', {
+    const cluster = new Cluster(this, "NautobotCluster", {
       containerInsights: true,
       vpc,
-      clusterName: 'NautobotCluster',
+      clusterName: "NautobotCluster",
     });
 
     cluster.addDefaultCloudMapNamespace({
@@ -33,61 +49,61 @@ export class NautobotFargateEcsStack extends Stack {
     });
 
     // Define a new IAM role for Fargate Task Definitions
-    const ecsTaskRole = new Role(this, 'ECSTaskRole', {
-      assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
+    const ecsTaskRole = new Role(this, "ECSTaskRole", {
+      assumedBy: new ServicePrincipal("ecs-tasks.amazonaws.com"),
     });
 
     // Attach the necessary managed policies
-    ecsTaskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
-    ecsTaskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLogsFullAccess'));
+    ecsTaskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"));
+    ecsTaskRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess"));
 
     // Define a new IAM role for your Fargate Service Execution
-    const ecsExecutionRole = new Role(this, 'ECSExecutionRole', {
-      assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
+    const ecsExecutionRole = new Role(this, "ECSExecutionRole", {
+      assumedBy: new ServicePrincipal("ecs-tasks.amazonaws.com"),
     });
 
     // Attach the necessary managed policies to your role
-    ecsExecutionRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
-    ecsExecutionRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLogsFullAccess'));
+    ecsExecutionRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"));
+    ecsExecutionRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess"));
 
     // Nautobot Worker
-    const nautobotWorkerTaskDefinition = new FargateTaskDefinition(this, 'NautobotWorkerTaskDefinition', {
+    const nautobotWorkerTaskDefinition = new FargateTaskDefinition(this, "NautobotWorkerTaskDefinition", {
       memoryLimitMiB: 4096,
       cpu: 2048,
       taskRole: ecsTaskRole,
       executionRole: ecsExecutionRole,
     });
     // Initialize the environment variable object
-    let environment: { [key: string]: string } = {        // Make sure to pass the database and Redis information to the Nautobot app.
-      'NAUTOBOT_DB_HOST': dbStack.postgresInstance.dbInstanceEndpointAddress,
-      'NAUTOBOT_REDIS_HOST': dbStack.redisCluster.attrRedisEndpointAddress,
+    let environment: { [key: string]: string } = {
+      // Make sure to pass the database and Redis information to the Nautobot app.
+      NAUTOBOT_DB_HOST: dbStack.postgresInstance.dbInstanceEndpointAddress,
+      NAUTOBOT_REDIS_HOST: dbStack.redisCluster.attrRedisEndpointAddress,
     };
 
     // update secrets w/ DB PW
-    secretsStack.secrets["NAUTOBOT_DB_PASSWORD"] = ecs.Secret.fromSecretsManager(dbStack.nautobotDbPassword, "password");
+    secretsStack.secrets["NAUTOBOT_DB_PASSWORD"] = ecs.Secret.fromSecretsManager(
+      dbStack.nautobotDbPassword,
+      "password"
+    );
 
-    const nautobotWorkerContainer = nautobotWorkerTaskDefinition.addContainer('nautobot-worker', {
+    const nautobotWorkerContainer = nautobotWorkerTaskDefinition.addContainer("nautobot-worker", {
       image: ContainerImage.fromDockerImageAsset(dockerStack.image),
-      logging: LogDrivers.awsLogs({ streamPrefix: 'NautobotWorker' }),
+      logging: LogDrivers.awsLogs({ streamPrefix: "NautobotWorker" }),
       environment: environment,
       secrets: secretsStack.secrets,
-      command: [
-        'nautobot-server',
-        'celery',
-        'worker',
-      ],
+      command: ["nautobot-server", "celery", "worker"],
       healthCheck: {
-        command: ['CMD', 'bash', '-c', 'nautobot-server celery inspect ping --destination celery@$HOSTNAME'],
+        command: ["CMD", "bash", "-c", "nautobot-server celery inspect ping --destination celery@$HOSTNAME"],
         interval: Duration.seconds(30),
         timeout: Duration.seconds(10),
         startPeriod: Duration.seconds(60),
         retries: 5,
-      }
+      },
     });
 
-    const workerService = new FargateService(this, 'NautobotWorkerService', {
+    const workerService = new FargateService(this, "NautobotWorkerService", {
       cluster,
-      serviceName: 'NautobotWorkerService',
+      serviceName: "NautobotWorkerService",
       enableExecuteCommand: true,
       taskDefinition: nautobotWorkerTaskDefinition,
       assignPublicIp: false,
@@ -96,35 +112,31 @@ export class NautobotFargateEcsStack extends Stack {
         subnetType: SubnetType.PRIVATE_WITH_EGRESS,
       },
       cloudMapOptions: {
-        name: 'nautobot-worker',
+        name: "nautobot-worker",
         cloudMapNamespace: cluster.defaultCloudMapNamespace,
         dnsRecordType: DnsRecordType.A,
       },
     });
 
     // Nautobot Scheduler Task Definition and Service
-    const nautobotSchedulerTaskDefinition = new FargateTaskDefinition(this, 'NautobotSchedulerTaskDefinition', {
+    const nautobotSchedulerTaskDefinition = new FargateTaskDefinition(this, "NautobotSchedulerTaskDefinition", {
       memoryLimitMiB: 4096,
       cpu: 2048,
       taskRole: ecsTaskRole,
       executionRole: ecsExecutionRole,
     });
 
-    const nautobotSchedulerContainer = nautobotSchedulerTaskDefinition.addContainer('nautobot-scheduler', {
+    const nautobotSchedulerContainer = nautobotSchedulerTaskDefinition.addContainer("nautobot-scheduler", {
       image: ContainerImage.fromDockerImageAsset(dockerStack.image),
-      logging: LogDrivers.awsLogs({ streamPrefix: 'NautobotScheduler' }),
+      logging: LogDrivers.awsLogs({ streamPrefix: "NautobotScheduler" }),
       environment: environment,
       secrets: secretsStack.secrets,
-      command: [
-        'nautobot-server',
-        'celery',
-        'beat',
-      ],
+      command: ["nautobot-server", "celery", "beat"],
     });
 
-    const schedulerService = new FargateService(this, 'NautobotSchedulerService', {
+    const schedulerService = new FargateService(this, "NautobotSchedulerService", {
       cluster,
-      serviceName: 'NautobotSchedulerService',
+      serviceName: "NautobotSchedulerService",
       enableExecuteCommand: true,
       taskDefinition: nautobotSchedulerTaskDefinition,
       assignPublicIp: false,
@@ -133,32 +145,32 @@ export class NautobotFargateEcsStack extends Stack {
         subnetType: SubnetType.PRIVATE_WITH_EGRESS,
       },
       cloudMapOptions: {
-        name: 'nautobot-scheduler',
+        name: "nautobot-scheduler",
         cloudMapNamespace: cluster.defaultCloudMapNamespace,
         dnsRecordType: DnsRecordType.A,
       },
     });
 
     // Nautobot App Task Definition and Service
-    const nautobotAppTaskDefinition = new FargateTaskDefinition(this, 'NautobotAppTaskDefinition', {
+    const nautobotAppTaskDefinition = new FargateTaskDefinition(this, "NautobotAppTaskDefinition", {
       memoryLimitMiB: 4096,
       cpu: 2048,
       taskRole: ecsTaskRole,
       executionRole: ecsExecutionRole,
     });
 
-    const nautobotAppContainer = nautobotAppTaskDefinition.addContainer('nautobot', {
+    const nautobotAppContainer = nautobotAppTaskDefinition.addContainer("nautobot", {
       image: ContainerImage.fromDockerImageAsset(dockerStack.image),
-      logging: LogDrivers.awsLogs({ streamPrefix: 'NautobotApp' }),
+      logging: LogDrivers.awsLogs({ streamPrefix: "NautobotApp" }),
       environment: environment,
       secrets: secretsStack.secrets,
       healthCheck: {
-        command: ['CMD-SHELL', 'curl -f http://localhost/health || exit 1'],
+        command: ["CMD-SHELL", "curl -f http://localhost/health || exit 1"],
         interval: Duration.seconds(30),
         timeout: Duration.seconds(10),
         startPeriod: Duration.seconds(60),
         retries: 5,
-      }
+      },
     });
 
     nautobotAppContainer.addPortMappings({
@@ -168,9 +180,9 @@ export class NautobotFargateEcsStack extends Stack {
       protocol: Protocol.TCP,
     });
 
-    const nginxContainer = nautobotAppTaskDefinition.addContainer('nginx', {
+    const nginxContainer = nautobotAppTaskDefinition.addContainer("nginx", {
       image: ContainerImage.fromDockerImageAsset(nginxStack.image),
-      logging: LogDrivers.awsLogs({ streamPrefix: 'Nginx' }),
+      logging: LogDrivers.awsLogs({ streamPrefix: "Nginx" }),
     });
 
     nginxContainer.addPortMappings({
@@ -180,9 +192,9 @@ export class NautobotFargateEcsStack extends Stack {
       protocol: Protocol.TCP,
     });
 
-    const nautobotAppService = new FargateService(this, 'NautobotAppService', {
+    const nautobotAppService = new FargateService(this, "NautobotAppService", {
       cluster,
-      serviceName: 'NautobotAppService',
+      serviceName: "NautobotAppService",
       enableExecuteCommand: true,
       taskDefinition: nautobotAppTaskDefinition,
       assignPublicIp: false,
@@ -193,20 +205,20 @@ export class NautobotFargateEcsStack extends Stack {
       securityGroups: [nautobotSecurityGroup],
       cloudMapOptions: {
         // This will be your service_name.namespace
-        name: 'nautobot-app',
+        name: "nautobot-app",
         cloudMapNamespace: cluster.defaultCloudMapNamespace,
         dnsRecordType: DnsRecordType.A,
       },
       serviceConnectConfiguration: {
         services: [
           {
-            portMappingName: 'nginx',
-            dnsName: 'nginx',
+            portMappingName: "nginx",
+            dnsName: "nginx",
             port: 80,
           },
           {
-            portMappingName: 'nautobot',
-            dnsName: 'nautobot',
+            portMappingName: "nautobot",
+            dnsName: "nautobot",
             port: 8080,
           },
         ],
@@ -214,20 +226,20 @@ export class NautobotFargateEcsStack extends Stack {
     });
 
     // Add necessary load balancer configuration
-    const listener = alb.addListener('Listener', {
+    const listener = alb.addListener("Listener", {
       port: 80,
       protocol: ApplicationProtocol.HTTP,
       // certificates: [] // Provide your SSL Certificates here if above is HTTP(S)
     });
 
-    listener.addTargets('NautobotAppService', {
+    listener.addTargets("NautobotAppService", {
       port: 8080,
       targets: [nautobotAppService],
       healthCheck: {
-        path: '/health',
+        path: "/health",
         port: "8080",
         interval: Duration.seconds(60),
-        healthyHttpCodes: '200,301',
+        healthyHttpCodes: "200,301",
       },
     });
   }
--- lib/nautobot-secrets-stack.ts
+++ lib/nautobot-secrets-stack.ts
@@ -1,10 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
-import * as dotenv from 'dotenv';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as fs from "fs";
+import * as path from "path";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as dotenv from "dotenv";
+import * as ecs from "aws-cdk-lib/aws-ecs";
 
 export class NautobotSecretsStack extends Stack {
   public readonly secrets: { [key: string]: ecs.Secret } = {};
@@ -12,8 +12,8 @@ export class NautobotSecretsStack extends Stack {
     super(scope, id, props);
 
     // Path to .env file
-    const objectKey = '.env';
-    const envFilePath = path.join(__dirname, '..', 'lib/secrets/', objectKey);
+    const objectKey = ".env";
+    const envFilePath = path.join(__dirname, "..", "lib/secrets/", objectKey);
 
     // Ensure .env file exists
     if (fs.existsSync(envFilePath)) {
@@ -26,21 +26,21 @@ export class NautobotSecretsStack extends Stack {
         const value = envConfig[key];
 
         // Check that the value exists and isn't undefined
-        if (value && value !== 'undefined') {
+        if (value && value !== "undefined") {
           // Create a new secret in Secrets Manager for this environment variable
           const secret = new secretsmanager.Secret(this, key, {
             secretName: key,
             generateSecretString: {
               secretStringTemplate: JSON.stringify({ value: value }),
-              generateStringKey: 'password',
+              generateStringKey: "password",
             },
           });
           // Add the secret to the secrets object
-          this.secrets[key] = ecs.Secret.fromSecretsManager(secret, 'value');
+          this.secrets[key] = ecs.Secret.fromSecretsManager(secret, "value");
         }
       }
     } else {
-      console.log('.env file does not exist');
+      console.log(".env file does not exist");
     }
   }
 }
